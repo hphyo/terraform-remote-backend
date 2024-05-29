d5dd246129b866d93d3f9334085326eacf06ff6d
hlaingphyohp <hlaingphyohp@outlook.com>
2024-05-29 11:15:00 +0700

    update versions file

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.51.1"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
-  profile       = var.aws_profile
-  region        = var.aws_region
-
+  # shared_config_files      = ["D:\\devopsboxes\\metallib-version-upgrade\\.aws\\conf"]
+  # shared_credentials_files = ["D:\\devopsboxes\\metallib-version-upgrade\\.aws\\creds"]
+  profile                 = var.aws_profile
+  region                  = var.aws_region
 }
